ecf44b0c314fa9025e0a6b8d214b0ae57f341027
Florent <FlorentCournoyer@proton.me>
2025-12-20 11:41:20 -0800

    Initial Commit

---
+++ CamTime2/CamTime2App.swift
@@ -0,0 +1,17 @@
+//
+//  CamTime2App.swift
+//  CamTime2
+//
+//  Created by user286406 on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct CamTime2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CamTime2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CamTime2
+//
+//  Created by user286406 on 12/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
